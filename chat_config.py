a37d9f35b792c7a49a6463052fa5adfcb625f5fa
alexkslv <31014728+alexkslv@users.noreply.github.com>
2018-02-19 01:59:30 +0700

    Updates

---
+++ chat_config.py
@@ -0,0 +1,8 @@
+app_main_icon       = 'icons/app_icon.png'
+app_exit_icon       = 'icons/app_exit.png'
+
+text_bold_icon      = 'icons/text_bold.png'
+text_bias_icon      = 'icons/text_bias.png'
+text_underline_icon = 'icons/text_underline.png'
+text_inc_icon       = 'icons/text_inc.png'
+text_dec_icon       = 'icons/text_dec.png'
